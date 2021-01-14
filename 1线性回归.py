07e873d00eec091aeef5138034d45fb6176b2588
zhaozhentao <344696734@qq.com>
2021-01-14 17:27:34 +0800

    在表格显示预测值

--- 1线性回归.py
+++ 1线性回归.py
@@ -5,13 +5,12 @@ import matplotlib.pyplot as plt
 # 随机创建100个x轴变量, 模拟生成有线性关系的数据
 x = np.random.random(50)
 # 加入随机的噪声
-noise = np.random.rand(50, ) / 100
+noise = np.random.rand(50, ) / 10
 # f(x) = 2x + 1
 y = 2 * x + 1 + noise
 
 # 显示x, y散点图
 plt.scatter(x, y)
-plt.show()
 
 model = tf.keras.Sequential([
     # 网络输出是1个value y, 输入是1个value x (y = wx + b)
@@ -25,7 +24,10 @@ model.summary()
 model.fit(x, y, epochs=3000)
 
 # 用训练完的模型来预测
-print(model.predict([1]))
+valid_x = [0.9, 0.8, 0.7]
+predict_y = model.predict(valid_x)
 
+print(predict_y)
 
-
+plt.scatter(valid_x, predict_y)
+plt.show()
