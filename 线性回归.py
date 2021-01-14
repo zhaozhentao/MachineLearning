35fe6e12bd29b031b27e7583d6f2037af5fa0f12
zhaozhentao <344696734@qq.com>
2021-01-14 16:49:40 +0800

    线性回归tf实现

--- 线性回归.py
+++ 线性回归.py
@@ -1,6 +1,31 @@
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
-x = np.random.random(100)
+# 随机创建100个x轴变量, 模拟生成有线性关系的数据
+x = np.random.random(50)
+# 加入随机的噪声
+noise = np.random.rand(50, ) / 100
+# f(x) = 2x + 1
+y = 2 * x + 1 + noise
 
-print(x)
+# 显示x, y散点图
+plt.scatter(x, y)
+plt.show()
+
+model = tf.keras.Sequential([
+    # 网络输出是1个value y, 输入是1个value x (y = wx + b)
+    tf.keras.layers.Dense(1, input_shape=(1,))
+])
+
+model.compile(optimizer='adam', loss='mse')
+# 查看网络参数
+model.summary()
+# 开始训练
+model.fit(x, y, epochs=3000)
+
+# 用训练完的模型来预测
+print(model.predict([1]))
+
+
+
