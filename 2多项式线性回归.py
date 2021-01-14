4cb4eb02f68cf300779bb26a92d40154b15b5187
zhaozhentao <344696734@qq.com>
2021-01-14 22:25:20 +0800

    多项式拟合

---
+++ 2多项式线性回归.py
@@ -0,0 +1,30 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+# 随机创建100个x轴变量, 模拟生成有线性关系的数据
+x = np.random.random(50)
+# 加入随机的噪声
+noise = np.random.rand(50, ) / 10
+# f(x) = 2 * x * x + 1
+y = 2 * x * x + 1 + noise
+
+# 显示x, y散点图
+plt.scatter(x, y)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(4, input_shape=(1,), activation='tanh'),
+    tf.keras.layers.Dense(2, activation='tanh'),
+    tf.keras.layers.Dense(1),
+])
+
+model.compile(optimizer='adam', loss='mse')
+# 开始训练
+model.fit(x, y, epochs=5000)
+
+# 用训练完的模型来预测
+valid_x = np.random.random(10)
+predict_y = model.predict(valid_x)
+
+plt.scatter(valid_x, predict_y)
+plt.show()
