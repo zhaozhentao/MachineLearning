b3a1bce68a035fc9f0e23898e2de70451be0845f
zhaozhentao <344696734@qq.com>
2021-01-15 07:59:50 +0800

    二次方程修改

--- 2多项式线性回归.py
+++ 2多项式线性回归.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 # 随机创建100个x轴变量, 模拟生成有线性关系的数据
 x = np.random.random(50)
 # 加入随机的噪声
-noise = np.random.rand(50, ) / 10
-# f(x) = 2 * x * x + 1
-y = 2 * x * x + 1 + noise
+noise = np.random.rand(50, ) / 50
+# f(x) = 2 * x * x - 2x + 1
+y = 2 * x * x - 2 * x + 1 + noise
 
 # 显示x, y散点图
 plt.scatter(x, y)
