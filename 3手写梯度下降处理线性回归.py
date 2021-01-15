6f9a0d001252c0cda8f7c777173694f100452511
zhaozhentao <344696734@qq.com>
2021-01-15 16:01:42 +0800

    加入loss

--- 3手写梯度下降处理线性回归.py
+++ 3手写梯度下降处理线性回归.py
@@ -10,6 +10,13 @@ def compute_gradient(current_w, current_b, training_x, training_y):
     return w_gradient, b_gradient
 
 
+# 计算均方差mean squared error  最小二乘法
+def mse(w, b, training_x, training_y):
+    N = float(len(training_x))
+    error = w * training_x + b - training_y
+    return (error * error).sum() / N
+
+
 # step1 产生模拟数据 y = 2x + 30
 train_x = np.random.random(50)
 train_y = 3 * train_x + 15
@@ -27,4 +34,5 @@ for i in range(6000):
     # 梯度下降更新w, b
     w = w - learning_rate * w_gradient
     b = b - learning_rate * b_gradient
-    print("loop: {}, w: {}, b: {}".format(i, w, b))
+    loss = mse(w, b, train_x, train_y)
+    print("loop: {}, w: {}, b: {}, loss: {}".format(i, w, b, loss))
