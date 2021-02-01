e1cf0c77aafe08f2abfc432e55982d33354a65d8
zhaozhentao <344696734@qq.com>
2021-02-01 15:37:24 +0800

    移除跟踪图片

--- 4ObjectDetect/1ObjectDetection.py
+++ 4ObjectDetect/1ObjectDetection.py
@@ -2,17 +2,21 @@ import pathlib
 
 import matplotlib.pyplot as plt
 import tensorflow as tf
-import tensorflow_datasets as tfds
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 
 def load_and_preprocess_image(path):
-    image = tf.io.read_file(path)
+    image = tf.io.read_file(path + '/img.png')
     image = tf.image.decode_jpeg(image, channels=3)
     image = tf.image.resize(image, [192, 192])
     image /= 255.0
-    return image
+
+    mask = tf.io.read_file(path + '/label.png')
+    mask = tf.image.decode_jpeg(mask, channels=3)
+    mask = tf.image.resize(mask, [192, 192])
+    mask /= 255.0
+    return image, mask
 
 
 data_root = pathlib.Path('/Users/zhaotao/Desktop/py/4ObjectDetect/data')
@@ -23,5 +27,7 @@ all_image_paths = [str(path) for path in all_image_paths]
 path_ds = tf.data.Dataset.from_tensor_slices(all_image_paths)
 image_ds = path_ds.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
 
+for n, image in enumerate(image_ds.take(1)):
+    plt.imshow(image[1])
 
-
+plt.show()
