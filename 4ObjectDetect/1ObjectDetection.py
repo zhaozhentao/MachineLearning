470544f89f78f86fcbabe6a1240c7ce13249690d
zhaozhentao <344696734@qq.com>
2021-02-01 16:13:19 +0800

    能够显示预测

--- 4ObjectDetect/1ObjectDetection.py
+++ 4ObjectDetect/1ObjectDetection.py
@@ -2,22 +2,76 @@ import pathlib
 
 import matplotlib.pyplot as plt
 import tensorflow as tf
-
-AUTOTUNE = tf.data.experimental.AUTOTUNE
+from tensorflow_examples.models.pix2pix import pix2pix
 
 
 def load_and_preprocess_image(path):
     image = tf.io.read_file(path + '/img.png')
     image = tf.image.decode_jpeg(image, channels=3)
-    image = tf.image.resize(image, [192, 192])
+    image = tf.image.resize(image, [128, 128])
     image /= 255.0
 
     mask = tf.io.read_file(path + '/label.png')
     mask = tf.image.decode_jpeg(mask, channels=3)
-    mask = tf.image.resize(mask, [192, 192])
+    mask = tf.image.resize(mask, [128, 128])
     mask /= 255.0
     return image, mask
 
+
+def unet_model(output_channels):
+    inputs = tf.keras.layers.Input(shape=[128, 128, 3])
+    x = inputs
+
+    # 在模型中降频取样
+    skips = down_stack(x)
+    x = skips[-1]
+    skips = reversed(skips[:-1])
+
+    # 升频取样然后建立跳跃连接
+    for up, skip in zip(up_stack, skips):
+        x = up(x)
+        concat = tf.keras.layers.Concatenate()
+        x = concat([x, skip])
+
+    # 这是模型的最后一层
+    last = tf.keras.layers.Conv2DTranspose(
+        output_channels, 3, strides=2,
+        padding='same')  # 64x64 -> 128x128
+
+    x = last(x)
+
+    return tf.keras.Model(inputs=inputs, outputs=x)
+
+
+def display(display_list):
+    plt.figure(figsize=(15, 15))
+
+    title = ['Input Image', 'True Mask', 'Predicted Mask']
+
+    for i in range(len(display_list)):
+        plt.subplot(1, len(display_list), i + 1)
+        plt.title(title[i])
+        plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
+        plt.axis('off')
+    plt.show()
+
+
+def create_mask(pred_mask):
+    pred_mask = tf.argmax(pred_mask, axis=-1)
+    pred_mask = pred_mask[..., tf.newaxis]
+    return pred_mask[0]
+
+
+def show_predictions(dataset=None, num=1):
+    if dataset:
+        for image, mask in dataset.take(num):
+            pred_mask = model.predict(image)
+            display([image[0], mask[0], create_mask(pred_mask)])
+    else:
+        display([sample_image, sample_mask, create_mask(model.predict(sample_image[tf.newaxis, ...]))])
+
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 data_root = pathlib.Path('/Users/zhaotao/Desktop/py/4ObjectDetect/data')
 
@@ -28,6 +82,43 @@ path_ds = tf.data.Dataset.from_tensor_slices(all_image_paths)
 image_ds = path_ds.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
 
 for image, mask in image_ds.take(1):
-    plt.imshow(mask)
+    sample_image, sample_mask = image, mask
 
-plt.show()
+display([sample_image, sample_mask])
+
+OUTPUT_CHANNELS = 3
+
+base_model = tf.keras.applications.MobileNetV2(input_shape=[128, 128, 3], include_top=False)
+
+# 使用这些层的激活设置
+layer_names = [
+    'block_1_expand_relu',  # 64x64
+    'block_3_expand_relu',  # 32x32
+    'block_6_expand_relu',  # 16x16
+    'block_13_expand_relu',  # 8x8
+    'block_16_project',  # 4x4
+]
+layers = [base_model.get_layer(name).output for name in layer_names]
+
+# 创建特征提取模型
+down_stack = tf.keras.Model(inputs=base_model.input, outputs=layers)
+
+down_stack.trainable = False
+
+up_stack = [
+    pix2pix.upsample(512, 3),  # 4x4 -> 8x8
+    pix2pix.upsample(256, 3),  # 8x8 -> 16x16
+    pix2pix.upsample(128, 3),  # 16x16 -> 32x32
+    pix2pix.upsample(64, 3),  # 32x32 -> 64x64
+]
+
+model = unet_model(OUTPUT_CHANNELS)
+model.compile(
+    optimizer='adam',
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy']
+)
+
+show_predictions()
+
+model_history = model.fit(image_ds, epochs=100)
